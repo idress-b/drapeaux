09223318720fa717e83b3272ccdc0d1a74322f79
idress-b <idress.benchellali@gmail.com>
2022-05-23 19:37:52 +0200

    filtrage style

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <div className="navigation">
+            <ul>
+                <li>
+                    <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : undefined)}> Accueil</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/a-propos" className={(nav) => (nav.isActive ? "nav-active" : undefined)}>A propos</NavLink>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Navigation;
---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const Abouts = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quia delectus minima sapiente excepturi saepe eaque id ea repudiandae facere optio at quos a exercitationem ullam esse recusandae quibusdam, nisi autem nulla praesentium fugiat dolores, alias eum. Ab laudantium minus enim, maiores veniam, delectus pariatur eveniet ad provident tempore impedit.</p>
+        </div>
+    );
+};
+
+export default Abouts;
