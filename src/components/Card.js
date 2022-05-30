c49e01fe9cb25ee04e00fcf727830ff5549caea1
idress-b <idress.benchellali@gmail.com>
2022-05-30 23:33:02 +0200

    gestion du responsif

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,18 +3,21 @@ import React, { useState } from "react";
 const Card = ({ country }) => {
   const [isDetailRendered, setIsDetailRendered] = useState(false);
   return (
-    <li className="card" onClick={(e) => setIsDetailRendered(true)}>
-      <img
-        src={country.flags.svg}
-        alt={"drapeau " + country.translations.fra.common}
-      />
-      <div className="infos">
-        <h2>{country.translations.fra.common}</h2>
-        <h4>{country.capital}</h4>
-        <p>Pop. {country.population.toLocaleString()}</p>
-      </div>
-      {isDetailRendered && <p>Click!!!</p>}
-    </li>
+    <>
+      
+      <li className="card" onClick={(e) => setIsDetailRendered(true)}>
+        <img
+          src={country.flags.svg}
+          alt={"drapeau " + country.translations.fra.common}
+        />
+        <div className="infos">
+          <h2>{country.translations.fra.common}</h2>
+          <h4>{country.capital}</h4>
+          <p>Pop. {country.population.toLocaleString()}</p>
+        </div>
+        {isDetailRendered && <p>Click!!!</p>}
+      </li>
+    </>
   );
 };
 
